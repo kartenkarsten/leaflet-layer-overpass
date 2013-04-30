20de9c7657e6614ed6128bad9374b9595ae443d9
Karsten Hinz <k.hinz@tu-bs.de>
2013-04-30 17:40:43 +0200

    new testing version/markers do not appear

--- customlayer.js
+++ customlayer.js
@@ -1,12 +1,41 @@
-var MyCustomLayer = L.Class.extend({
+L.CustomLayer = L.Class.extend({
+  options: {
+    apiUrl: 'http://www.jugglingedge.com/feeds/createjson.php?CallBack=?',
+    userID: "404",
+    icons:{"juggling":
+      L.icon({
+        iconUrl: 'icons/conTag_1.png',
+        iconSize: [32, 32],
+        iconAnchor: [-32, 0],
+        popupAnchor: [0, 0]
+      }),
+      "unicycle": L.icon({
+        iconUrl: 'icons/conTag_2.png',
+        iconSize: [32, 32],
+        iconAnchor: [0, 35],
+        popupAnchor: [0, 0]
+      }),
+      "acrobatics": L.icon({
+        iconUrl: 'icons/conTag_3.png',
+        iconSize: [32, 32],
+        iconAnchor: [0, 32],
+        popupAnchor: [0, 0]
+      })
+    },
+    latlng: new L.LatLng(50, 4)
+  },
 
-  initialize: function (latlng) {
+  initialize: function (options) {
+    L.Util.setOptions(this, options);
     // save position of the layer or any options from the constructor
-    this._latlng = latlng;
+    console.log("init");
   },
 
     onAdd: function (map) {
       this._map = map;
+      console.log("G");
+      this._loadPoi();
+      
 
       // create a DOM element and put it into one of the map panes
       this._el = L.DomUtil.create('div', 'my-custom-layer leaflet-zoom-hide');
@@ -25,8 +54,33 @@ var MyCustomLayer = L.Class.extend({
 
     _reset: function () {
       // update layer's position
-      var pos = this._map.latLngToLayerPoint(this._latlng);
-      L.DomUtil.setPosition(this._el, pos);
-    }
-});
+      var pos = this._map.latLngToLayerPoint(this.options.latlng);
+      //L.DomUtil.setPosition(this._el, pos);
+    },
 
+    _loadPoi: function () {
+      console.log("load Pois");
+        $.getJSON(this.apiUrl, {Data : "events", UserID : this.userID } , function(data) {
+          //convert Conventions to a POI overlay
+          for(i=0;i<data.length;i++) {
+            var event = data[i];
+
+            if (event.EventID in this.ids) return;
+            this.ids[event.EventID] = event;
+            if ((event.Lat == 0) && (event.Lng == 0)) {
+              console.log(event.ShortTitle + " has no position");
+              continue;
+            }
+            var pos = new L.LatLng(event.Lat, event.Lng);
+            var icon = jongIcon;
+            var popup = poiInfo(event);
+            var marker = new L.Marker(pos, {icon: icon}).bindPopup(popup);
+            this._map.addLayer(marker);
+          }
+        });
+    }
+
+});
+//L.CustomLayer = function (options) {
+//  return new L.CustomLayer(options);
+//}
