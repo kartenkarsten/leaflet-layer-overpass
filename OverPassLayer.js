c664faa11952d7885d699e7503dd2733cddaf83a
Karsten Hinz <k.hinz@tu-bs.de>
2013-07-07 22:02:40 +0200

    added cacheable overpass requests

--- OverPassLayer.js
+++ OverPassLayer.js
@@ -142,19 +142,85 @@ L.OverPassLayer = L.FeatureGroup.extend({
     return link + $('<div>').append(r).html();
   },
 
-  onMoveEnd: function () {
-    console.log("load Pois");
+  /**
+   * splits the current view in uniform bboxes to allow caching
+   */
+  long2tile: function (lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); },
+  lat2tile: function (lat,zoom)  {
+      return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); 
+  },
+  tile2long: function (x,z) {
+      return (x/Math.pow(2,z)*360-180);
+  },
+  tile2lat: function (y,z) {
+      var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
+      return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+  },
+  _view2BBoxes: function(l,b,r,t) {
+      //console.log(l+"\t"+b+"\t"+r+"\t"+t);
+      this.addBBox(l,b,r,t);
+      //console.log("calc bboxes");
+      var requestZoomLevel= 14;
+      //get left tile index
+      var lidx = this.long2tile(l,requestZoomLevel);
+      var ridx = this.long2tile(r,requestZoomLevel);
+      var tidx = this.lat2tile(t,requestZoomLevel);
+      var bidx = this.lat2tile(b,requestZoomLevel);
 
-    if (this._map.getZoom() >= this.options.minzoom) {
-        $.ajax({
-          url: this.options.query.replace(/(BBOX)/g, this._map.getBounds().toOverpassBBoxString()),
-          context: { instance: this },
-          crossDomain: true,
-          dataType: "json",
-          data: {},
-          success: this.options.callback
-        });
-    }
+      //var result;
+      var result = new Array();
+      for (var x=lidx; x<=ridx; x++) {
+          for (var y=tidx; y<=bidx; y++) {//in tiles tidx<=bidx
+              var left = this.tile2long(x,requestZoomLevel);
+              var right = this.tile2long(x+1,requestZoomLevel);
+              var top = this.tile2lat(y,requestZoomLevel);
+              var bottom = this.tile2lat(y+1,requestZoomLevel);
+              //console.log(left+"\t"+bottom+"\t"+right+"\t"+top);
+              this.addBBox(left,bottom,right,top);
+              //console.log("http://osm.org?bbox="+left+","+bottom+","+right+","+top);
+              result.push( new L.LatLngBounds(new L.LatLng(bottom, left),new L.LatLng(top, right)));
+          }
+      }
+      //console.log(result);
+      return result;
+  },
+
+  addBBox: function (l,b,r,t) {
+      var polygon = L.polygon([
+              [t, l],
+              [b, l],
+              [b, r],
+              [t, r]
+              ]).addTo(this._map);
+  },
+
+  onMoveEnd: function () {
+      console.log("load Pois");
+      //console.log(this._map.getBounds());
+      if (this._map.getZoom() >= this.options.minzoom) {
+          //var bboxList = new Array(this._map.getBounds());
+          var bboxList = this._view2BBoxes(
+                  this._map.getBounds()._southWest.lng,
+                  this._map.getBounds()._southWest.lat,
+                  this._map.getBounds()._northEast.lng,
+                  this._map.getBounds()._northEast.lat);
+
+
+          console.log("erqest pois");
+          for (var i=0; i<bboxList.length; i++) {
+              var bbox = bboxList[i];
+
+              //console.log(bbox);
+              $.ajax({
+                  url: this.options.query.replace(/(BBOX)/g, bbox.toOverpassBBoxString()),
+                  context: { instance: this },
+                  crossDomain: true,
+                  dataType: "json",
+                  data: {},
+                  success: this.options.callback
+              });
+          }
+      }
   },
 
   onAdd: function (map) {
